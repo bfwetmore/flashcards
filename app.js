90bc60eb683c1a12cf4feed745204b53a0a1b629
venomm <bfwetmore@gmail.com>
2022-03-21 16:48:46 -0400

    WIP

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send('I love Nina, and emi, and food')
+} );
+
+
+console.log("Server successfully started")
+//app.listen(3000);
